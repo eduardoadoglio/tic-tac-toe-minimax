76b207ff3e0f1f9a845d971ed20f547d76b55bd5
eduardoadoglio <eduardoadoglio4@gmail.com>
2021-02-28 23:08:54 -0300

    Remove commented variable

--- src/main.go
+++ src/main.go
@@ -3,7 +3,6 @@ package main
 
 func main() {
 	gui := NewGUI()
-	// baseInterface := setupBaseGameInterface(gameManager)
 	baseInterface := gui.setupMenuInterface()
 	gui.setWindowContent(baseInterface)
 	gui.showContentAndRun()
